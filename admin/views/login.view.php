eb3ac64fee33b961cafc27532fcba8d7b57a3b59
Lesthuuur <104022740+Lesthuuur@users.noreply.github.com>
2024-10-23 22:13:07 +0800

    first commit create login page

---
+++ admin/views/login.view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="/styles/login.css">
+</head>
+<body>
+    <main>
+        <div class="left">
+            <h1>EMPLOYEE MANAGEMENT SYSTEM</h1>
+        </div>
+        
+
+        <div class="right">
+            <form action="" method="POST">
+
+                <h1>LOGIN</h1>
+                <div class="line-division"></div>
+
+                <input type="text" class="form-input username" id="username" name="username" placeholder="USERNAME">
+                <input type="text" class="form-input password" id="password" name="password" placeholder="PASSWORD">
+
+                <input type="submit" class="submit-btn" id="submit" name="submit" value="LOGIN">
+
+            </form>
+        </div>
+    </main>
+</body>
+</html>
